198be21b3cc3927456d39646943dfff988204345
Mario von Rickenbach <mario@mariov.ch>
2022-04-29 10:31:16 +0200

    delay positioning after PCF was localized

--- Assets/Scripts/RoomDebugger/RoomTransform.cs
+++ Assets/Scripts/RoomDebugger/RoomTransform.cs
@@ -1,7 +1,38 @@
+using System;
 using UnityEngine;
+using UnityEngine.XR.MagicLeap;
 
 public class RoomTransform : MonoBehaviour
 {
+	bool delayActivation => !Application.isEditor;
+	void Awake()
+	{
+		if(delayActivation)
+		{
+			gameObject.SetActive(false);
+
+			MLPersistentCoordinateFrames.OnLocalized += HandleOnLocalized;
+		}
+	}
+
+	void OnDestroy()
+	{
+		if (delayActivation)
+		{
+			MLPersistentCoordinateFrames.OnLocalized -= HandleOnLocalized;
+		}
+	}
+
+	void HandleOnLocalized(bool localized)
+	{
+		if(localized)
+		{
+			Update();
+			gameObject.SetActive(true);
+		}
+	}
+
+
 	void Update()
 	{
 		var roomTransform = RoomTracking.instance.roomTransform;
